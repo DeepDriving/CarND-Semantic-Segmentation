5d73c50616a9829d6d2ea899d6f6674190dd1bb0
DeepDriving <tawbay@gmail.com>
2017-08-14 13:12:21 -0700

    Add files via upload
    1st commit

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ def load_vgg(sess, vgg_path):
     """
     # TODO: Implement function
     #   Use tf.saved_model.loader.load to load the model and weights
+    
+
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
@@ -33,7 +35,16 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    return None, None, None, None, None
+    tf.saved_model.loader.load(sess, ['vgg16'], vgg_path)
+    graph = tf.get_default_graph()
+
+    input_image = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    return input_image,keep_prob, layer3_out, layer4_out, layer7_out
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,7 +58,45 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+    
+    
+    #Encoder
+    #Fully connected 1x1 convoluation with layer 7 output
+    #layer7_fcn = tf.layers.conv2d(vgg_layer7_out, layer7_filters, (1,1), (1,1), padding='SAME',
+     #                           kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
+
+    layer7_fcn = tf.layers.conv2d(vgg_layer7_out, num_classes, (1,1), (1,1), 
+                                kernel_initializer=tf.truncated_normal_initializer( stddev=0.01 ))
+    #Decoder
+
+    #1st Upsampling
+    layer7_upscale = tf.layers.conv2d_transpose(layer7_fcn, num_classes,(4,4), (2,2), padding='SAME',
+                                kernel_initializer=tf.truncated_normal_initializer(stddev=0.01) )
+
+    #Fully Connected  1x1 convoluation with layer 4 output
+    layer4_fcn = tf.layers.conv2d(vgg_layer4_out, num_classes, (1,1), (1,1),
+                                kernel_initializer=tf.truncated_normal_initializer(stddev=0.01) )
+
+    #Skip connection
+    layer4_skip = tf.add(layer7_upscale, layer4_fcn)
+
+    #2nd upsampling
+    layer4_upscale = tf.layers.conv2d_transpose(layer4_skip, num_classes, (4,4), (2,2), padding='SAME',
+                                kernel_initializer=tf.truncated_normal_initializer(stddev = 0.01))
+
+    #Full conneced 1x1 convolution with layer 3 output
+    layer3_fcn = tf.layers.conv2d(vgg_layer3_out, num_classes, (1,1), (1,1), 
+                                kernel_initializer=tf.truncated_normal_initializer(stddev = 0.01))
+
+    #Skip connection
+    layer3_skip = tf.add(layer4_upscale, layer3_fcn)
+
+    #Output by upsampling last layer
+    output = tf.layers.conv2d_transpose(layer3_skip, num_classes, (16,16), (8,8), padding='SAME',
+                                kernel_initializer=tf.truncated_normal_initializer(stddev = 0.01))
+
+
+    return output
 tests.test_layers(layers)
 
 
@@ -61,7 +110,17 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+
+
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    labels = tf.reshape(correct_label, (-1, num_classes))
+    
+    entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels = labels)
+    loss = tf.reduce_mean(entropy)
+
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+    return logits, train_op, loss
 tests.test_optimize(optimize)
 
 
@@ -81,15 +140,32 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-    pass
+    for epoch in range(epochs):
+
+        for image, gt_image in get_batches_fn(batch_size):
+            feed_dict = {
+                input_image: image,
+                correct_label: gt_image,
+                keep_prob: 0.5,
+                learning_rate: 0.0001                     
+            }
+        _,loss = sess.run([train_op, cross_entropy_loss], feed_dict= feed_dict)
+        print("Epoch: {}/{}, Loss {}".format(epoch, epochs, loss))   
 tests.test_train_nn(train_nn)
 
+def save_model(sess):
+    saver = tf.train.Saver()
+    saver.save(sess,'/media/annie.guan/DATA/Data/Kitti/KittiSeg/DATA/model.ckpt')
+    saver.export_meta_graph('/media/annie.guan/DATA/Data/Kitti/KittiSeg/DATA/model.meta')
+    tf.train.write_graph(sess.graph_def, "/media/annie.guan/DATA/Data/Kitti/KittiSeg/DATA/", "model.pb", False)
 
 def run():
     num_classes = 2
+    epochs = 80
+    batch_size = 16
     image_shape = (160, 576)
-    data_dir = './data'
-    runs_dir = './runs'
+    data_dir = '/media/annie.guan/DATA/Data/Kitti/KittiSeg/DATA'
+    runs_dir = '/media/annie.guan/DATA/Data/Kitti/KittiSeg/RUNS'
     tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
@@ -116,6 +192,26 @@ def run():
         #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
+
+        
+        
+        input_image,keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess,vgg_path)
+
+        output = layers(layer3_out, layer4_out, layer7_out, num_classes)
+        label = tf.placeholder(dtype=tf.float32,shape=[None, None, None, 2])
+        learning_rate = tf.placeholder(dtype=tf.float32)
+        
+        logits, train_op, loss = optimize(output,label,learning_rate,num_classes)
+
+        sess.run(tf.global_variables_initializer())
+        
+        train_nn(sess,epochs,batch_size, get_batches_fn, train_op, loss,input_image, 
+                 label, keep_prob, learning_rate)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+
+        #save the model
+        save_model(sess)
+
 
 
 if __name__ == '__main__':
